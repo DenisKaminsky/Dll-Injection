d830c9a11e255e11abc08346245c0d85439775d3
DenisKaminsky <deniskaminsky123@mail.ru>
2018-11-18 17:51:14 +0300

    add string scan

---
+++ Dll Injection/Source.cpp
@@ -0,0 +1,50 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int n = 5; //strings count
+string* stringArray;
+
+void InitializeArray()
+{
+	string s = "";
+
+	stringArray = new string[n];
+	for (int i = 0; i < n; i++)
+	{
+		s = "sometext" + to_string(i+1);
+		stringArray[i] = s;
+	}
+}
+
+void DeleteObjects()
+{
+	delete[] stringArray;
+
+}
+
+void memScan()
+{
+	cout << "Initial array: \n" << endl;
+	for (int i = 0; i < n; i++)
+		cout << stringArray[i]<< endl;
+	cout << "" << endl;
+	
+	//Replace(stringArray[3].c_str(), "DenisKaminsky");
+	cout << "Final array: \n" << endl;
+	for (int i = 0; i < n; i++)
+		cout << stringArray[i]<< endl;
+	cout << "" << endl;
+}
+
+void main()
+{
+	InitializeArray();
+	//Scan memory
+	memScan();
+
+	DeleteObjects();
+}
